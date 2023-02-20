fa228fea8f83c29d07eae185bd11eda0d8f406d8
Anjani Kumar <giri4841@gmail.com>
2023-02-20 19:36:49 +0530

    Build

---
+++ src/screens/Controller.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Home from '../screens/home/Home';
+import Details from '../screens/details/Details';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import BookShow from '../screens/bookshow/BookShow';
+import Confirmation from '../screens/confirmation/Confirmation';
+
+class Controller extends Component {
+
+  constructor() {
+    super();
+    this.baseUrlNew = "http://localhost:8085/api/v1/";
+  }
+  render() {
+    return (
+      <Router>
+        <div className="main-container">
+          <Route exact path='/' render={(props) => <Home {...props}  baseUrl={this.baseUrlNew} />} />
+          <Route path='/movie/:id' render={(props) => <Details {...props} baseUrl={this.baseUrlNew} />} />
+          <Route path='/bookshow/:id' render={(props) => <BookShow {...props} baseUrl={this.baseUrlNew} />} />
+          <Route path='/confirm/:id' render={(props) => <Confirmation {...props} baseUrl={this.baseUrlNew} />} />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default Controller;
